28c85c0f2be5b3f4ebe3bbcfa3159670baac8d72
Maksym Marushkevych <maksym.marushkevych@gmail.com>
2022-02-04 08:58:43 +0200

    Dummy properties

--- example/App.tsx
+++ example/App.tsx
@@ -34,19 +34,10 @@ function App() {
     <SafeAreaView style={styles.container}>
       <KYCAID
         config={{
-          // api_url: 'https://api.kycaid.com',
-          // api_token: 'bc9d9bad1c355148071abab5fca77264a02f',
-          // form_id: '7c87abe91c56614f9018d30533f67a385b42',
-            // api_token: '03762a190801c54e3c2ae8d84cc6abbc8afe',
-            // form_id: '25579bf50d4d4144291a12a7bab6e7a7af8b',
-          api_url: 'https://stg-api.kycaid.com',
-          api_token: '8d861e830717f648f11a27d888ff11f05616',
-          form_id: 'c029792f042e324a230a70d30930dd56da6d',
-          // applicant_id: '9bbf9d4c041e41473918c7176b1c263691cc',
-            // api_url: 'https://192.168.0.115:3005',
-            // api_token: '03762a190801c54e3c2ae8d84cc6abbc8afe',
-            // form_id: '41b0f81402fff14a9e2bc6b4c1d11ff844b5',
-            response_url: 'https://www.google.com/',
+            api_url: '<api_url>',
+            api_token: '<api_token>',
+            form_id: '<form_id>',
+            response_url: '<response_url>',
         }}
         verificationCallback={handleVerificationCallback}
       />
